482a0df57af6d1c706dacad34d737c2a5f8d6c0c
catjunior <mingzhi.wu12@gmail.com>
2017-12-01 15:05:39 -0500

    hello controller

---
+++ SpringRest/src/main/java/RestService/Controllers/Controller.java
@@ -0,0 +1,13 @@
+package RestService.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return "hello";
+    }
+}
